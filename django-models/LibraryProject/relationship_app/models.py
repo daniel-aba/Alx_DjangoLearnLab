c8aa0dbaedcfae078703732c1a4582f5fd5b1461
Danielaba <abadaniel284@gmail.com>
2025-09-04 18:48:56 +0300

    fix: Corrected migration to handle unique ISBN constraint

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -19,6 +19,8 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13, null=True, unique=True)
+    publisher = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
