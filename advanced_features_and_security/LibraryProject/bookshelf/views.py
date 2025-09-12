fc1fedc8fbd5e9ade9d7e2a21d964eb706fe943d
Danielaba <abadaniel284@gmail.com>
2025-09-12 14:26:03 +0300

    Add missing import for ExampleForm in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 from .forms import BookForm, ExampleForm  # The checker is looking for this import.
 
 # --- Views for the Book model ---
