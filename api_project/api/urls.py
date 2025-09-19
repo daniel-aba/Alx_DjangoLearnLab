754b23e132fe331d1098904949dd0934a3d67c10
Danielaba <abadaniel284@gmail.com>
2025-09-19 12:04:24 +0300

    feat: Add token authentication and permissions to secure API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,15 @@
-# api_project/api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views # Add this line
 from .views import BookViewSet
 
-# Create a router and register our viewsets with it.
+# Create a router instance
 router = DefaultRouter()
-router.register(r'books', BookViewSet, basename='book')
 
-# The API URLs are now determined automatically by the router.
+# Register the ViewSet with the router
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token), # Add this line
 ]
