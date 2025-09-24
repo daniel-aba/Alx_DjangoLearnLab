b6637548a07311dee1f1ca43cf2ce2e05f2aa41d
Danielaba <abadaniel284@gmail.com>
2025-09-24 11:59:15 +0300

    feat: Rectify Django DRF project setup with custom serializers

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
