f06ba8d75d6f587cc67ce71f8ac06c3e2ae3bf87
Danielaba <abadaniel284@gmail.com>
2025-09-24 12:20:07 +0300

    feat: Add initial project setup with models and serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,5 @@
+# api/models.py
+
 from django.db import models
 
 class Author(models.Model):
@@ -8,8 +10,8 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
+    publication_year = models.IntegerField(default=2023)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    publication_date = models.DateField()
 
     def __str__(self):
         return self.title
