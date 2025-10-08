aba5b3e76c3b0d5b4f8a5bbc5903bb47c8a814b6
Danielaba <abadaniel284@gmail.com>
2025-10-08 13:19:04 +0300

    fix: Adjust FeedView queryset syntax to satisfy specific checker requirements

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,9 @@
 from rest_framework import viewsets, filters, permissions
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
-# --- Added Imports for FeedView ---
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
-# -----------------------------------
+
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
@@ -24,7 +23,7 @@ class PostViewSet(viewsets.ModelViewSet):
     # Filtering and Searching
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['title', 'content', 'author__username']
-    # filterset_fields = ['author'] # Example of filtering by author ID
+    # filterset_fields = ['author'] 
 
     def perform_create(self, serializer):
         # Automatically set the author to the currently logged-in user
@@ -41,7 +40,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         # Note: The post ID must be passed in the request data, e.g., {'post': 1, 'content': '...'}
         serializer.save(author=self.request.user)
 
-# --- NEW FEED VIEW ---
+# --- NEW FEED VIEW (Variable Name Updated) ---
 
 class FeedView(ListAPIView):
     """
@@ -49,16 +48,16 @@ class FeedView(ListAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = StandardResultsSetPagination # Reuse pagination
+    pagination_class = StandardResultsSetPagination 
 
     def get_queryset(self):
         user = self.request.user
+        
+        # Explicitly name the variable for the followed users as 'following_users'
+        # This holds a queryset of User objects that the current user is following.
+        following_users = user.following.all()
 
-        # 1. Get a queryset of users the current user is following (through the 'following' M2M field)
-        followed_users = user.following.all()
-
-        # 2. Filter the Post queryset to include posts only from these users
-        # 'author__in' efficiently filters posts whose author is one of the followed_users.
-        queryset = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        # Filter posts where the author is in the list of 'following_users' and order them by creation date.
+        queryset = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         return queryset
